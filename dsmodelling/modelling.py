b9ef64a8fb1604e614219b25b97d388d92d11256
alexj14 <alexj14@vt.edu>
2024-02-21 19:48:13 -0500

    basic word frequency function working

--- dsmodelling/modelling.py
+++ dsmodelling/modelling.py
@@ -1,6 +1,9 @@
 import nltk
+
 def wordcloud(text):
-    words = nltk.tokenize.word_tokenize(text, num)
+    num = 3
+    words = nltk.tokenize.word_tokenize(text)
     stopwords = nltk.corpus.stopwords.words('english')
     wordDist = nltk.FreqDist(w.lower() for w in words if w not in stopwords)
     return(wordDist.most_common(num))
+print(wordcloud('hello hello world'))
